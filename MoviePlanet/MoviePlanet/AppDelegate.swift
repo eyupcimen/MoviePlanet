1a193284d7e54a4cf0bd20fa3600da7fb9e58005
eyupcimenipucu <eyupcimen@ipucubilisim.com.tr>
2021-09-23 00:18:18 +0300

    no message

---
+++ MoviePlanet/MoviePlanet/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MoviePlanet
+//
+//  Created by eyup cimen on 22.09.2021.
+//
+
+import UIKit
+
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ MoviePlanet/MoviePlanet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviePlanet
+//
+//  Created by eyup cimen on 22.09.2021.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
